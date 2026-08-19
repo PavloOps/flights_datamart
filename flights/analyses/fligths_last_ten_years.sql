a9cf44a70b1438b11129682f67e06e1f48029156
Pavlova <data_analitik@s-zdorovie.ru>
2026-08-19 11:01:09 +0300

    lecture 8.1

--- flights/analyses/fligths_last_ten_years.sql
+++ flights/analyses/fligths_last_ten_years.sql
@@ -1,5 +1,5 @@
-{% set current_date = (run_started_at | string)[:10] %}
-{% set years_delta = 10 %}
+{%- set current_date = (run_started_at | string)[:10] -%}
+{%- set years_delta = 10 %}
 
 
 SELECT
@@ -10,3 +10,33 @@ WHERE a1.Scheduled_departure BETWEEN
     '{{ current_date }}'::DATE - INTERVAL '{{ years_delta }} years'
 AND '{{ current_date }}'::DATE
 GROUP BY a1.Scheduled_departure
+
+
+{%- set source_relation = adapter.get_relation(
+    database="dwh_flights",
+    schema="intermediate",
+    identifier="fct_flights"
+) %}
+
+{{ source_relation.database }}
+{{ source_relation.schema }}
+{{ source_relation.identifier }}
+{{ source_relation.is_table }}
+{{ source_relation.is_view }}
+{{ source_relation.is_cte }}
+
+{% set source_relation = load_relation(ref("fct_flights")) %}
+
+{% set source_relation = api.Relation.create(
+    database="dwh_flights",
+    schema="intermediate",
+    identifier="fct_flights",
+    type="table"
+) %}
+
+{% set columns = adapter.get_columns_in_relation(source_relation) %}
+
+{% for column in columns -%}
+    {{ 'Columns: ' ~ column}}
+{% endfor %}
+
--- flights/models/staging/flights/stg_flights__airports.sql
+++ flights/models/staging/flights/stg_flights__airports.sql
@@ -1,14 +1,24 @@
 {{
     config(
-        materialized = 'table'
+        materialized = 'table',
+        post_hook = '
+            {% set backup_relation = api.Relation.create(
+                    database = this.database,
+                    schema = this.schema,
+                    identifier = this.identifier ~ "_dbt_backup_new",
+                    type = "table"
+                ) 
+            %}
+            {% do adapter.drop_relation(backup_relation) %}
+            {% do adapter.rename_relation(this, backup_relation) %}
+        '
     )
 }}
-
-select
-  {{ adapter.quote("airport_code") }},
-  {{ adapter.quote("airport_name") }},
-  {{ adapter.quote("city") }},
-  {{ adapter.quote("coordinates") }},
-  {{ adapter.quote("timezone") }}
-
-from {{ source('demo_src', 'airports') }}
+SELECT
+    airport_code as {{ adapter.quote('airport_code') }},
+    airport_name as {{ adapter.quote('airport_name') }},
+    city as {{ adapter.quote('city') }},
+    coordinates as {{ adapter.quote('coordinates') }},
+    timezone as {{ adapter.quote('timezone') }}
+FROM
+    {{ source('demo_src', 'airports') }}
